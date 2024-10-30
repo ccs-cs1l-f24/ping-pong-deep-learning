3ad9f527b1b0770132ab35afc95a34f07ac3fe62
Dylan Lu <dylanelu@gmail.com>
2024-10-29 17:47:16 -0700

    add next next

--- src/main.py
+++ src/main.py
@@ -2,3 +2,16 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
 results = model.train(data="../dataset/opentt.yaml", epochs=3, imgsz=640) # TODO: Play with epochs and imgsz
+
+
+
+
+"""
+TODO: Fix the datasets dir
+- put the downloaded videos somewhere like opentt/original
+- put the images and labels dir in opentt
+- put the yaml file in the right spot and make it reference opentt
+- AHHHHH
+
+
+"""
